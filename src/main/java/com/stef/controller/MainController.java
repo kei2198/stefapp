9afce590ff42f314c7d3c768b033d8c41497e659
codesalotl <jaspertoribio10@gmail.com>
2024-10-29 15:28:57 +0800

    Initial commit edit main

--- src/main/java/com/stef/controller/MainController.java
+++ src/main/java/com/stef/controller/MainController.java
@@ -1,2 +1,14 @@
-package com.stef.controller;public class MainController {
+package com.stef.controller;
+
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
+public class MainController {
+    @FXML
+    private TextField firstname;
+    @FXML
+    private TextField middlename;
+    @FXML
+    private TextField lastname;
 }
